7876ff589c4a1a7ecd3394e68188f0b4ca797114
桜火 <xie@loli.fit>
2021-02-26 13:37:58 +0800

    初始化哔哩哔哩动态模块
    Signed-off-by: 桜火 <xie@loli.fit>

---
+++ bilibili_dynamic/bilibili_dynamic/bilibili_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 桜火, Inc. All Rights Reserved 
+#
+# @Time    : 2021/2/26 13:37
+# @Author  : 桜火
+# @Email   : xie@loli.fit
+# @File    : bilibili_test.py
+# @Software: PyCharm
